443e986e8576a3964d7549c0eeb60b975883ac03
rn <rn@gmail.com>
2025-03-17 13:48:56 +0700

    fixing: confirmation input

--- obj/Debug/net9.0/DefectRecord.AssemblyInfo.cs
+++ obj/Debug/net9.0/DefectRecord.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DefectRecord")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a6a23a4f616c6af0350ff0dccbf050fbdea4a02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f4e4cba8eb5f255e1cac48ae077fb97ae97b20e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DefectRecord")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DefectRecord")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
